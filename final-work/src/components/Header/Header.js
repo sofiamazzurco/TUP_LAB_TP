fcffbcc79560312c7c19ce77ba2af6a194b1f276
amentha <amentha2908@gmail.com>
2023-05-29 14:07:19 -0300

    modificaciones

--- final-work/src/components/Header/Header.js
+++ final-work/src/components/Header/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
         <div className='header'>
             <h1>UTN</h1>
             <div className='btn'>
-                <button className='btn btn-primary'>Log in</button>
-                <button className='btn btn-light'>Sing in</button>
+                <button className='btn btn-primary'>Log In</button>
+                <button className='btn btn-light'>Sign In</button>
             </div>
         </div>
     );
